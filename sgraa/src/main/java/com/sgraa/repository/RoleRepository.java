31e34d437985f3744580cf9776b30efa59f8d848
Mateus Araujo <163949052+MQUARTZO@users.noreply.github.com>
2025-02-22 07:53:37 -0300

    Update RoleRepository.java

--- sgraa/src/main/java/com/sgraa/repository/RoleRepository.java
+++ sgraa/src/main/java/com/sgraa/repository/RoleRepository.java
@@ -2,7 +2,11 @@ package com.sgraa.repository;
 
 import com.sgraa.model.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+/**
+ * Interface que define o repositório para a entidade "RolerRepository".
+ * Esta interface estende JpaRepository, fornecendo métodos CRUD padrão e permitindo a criação de consultas personalizadas.
+ * Repositórios são responsáveis por abstrair o acesso ao banco de dados e fornecer operações de persistência para a entidade.
+ */
 public interface RoleRepository extends JpaRepository<Role, Long> {
     Role findByNome(String nome);
 }
