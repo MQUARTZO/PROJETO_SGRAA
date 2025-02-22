9ffff05245a2860ceeb9e8b8a40d079fa33a9f7d
Mateus Araujo <163949052+MQUARTZO@users.noreply.github.com>
2025-02-22 07:52:41 -0300

    Update PretendenteRepository.java

--- sgraa/src/main/java/com/sgraa/repository/PretendenteRepository.java
+++ sgraa/src/main/java/com/sgraa/repository/PretendenteRepository.java
@@ -2,6 +2,10 @@ package com.sgraa.repository;
 
 import com.sgraa.model.Pretendente;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+/**
+ * Interface que define o repositório para a entidade "PretendenteRepository".
+ * Esta interface estende JpaRepository, fornecendo métodos CRUD padrão e permitindo a criação de consultas personalizadas.
+ * Repositórios são responsáveis por abstrair o acesso ao banco de dados e fornecer operações de persistência para a entidade.
+ */
 public interface PretendenteRepository extends JpaRepository<Pretendente, Long> {
 }
