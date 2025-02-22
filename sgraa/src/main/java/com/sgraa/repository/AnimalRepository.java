7d852b4fd4597e43de45cbb8f8efaa76ff42782f
Mateus Araujo <163949052+MQUARTZO@users.noreply.github.com>
2025-02-22 07:50:46 -0300

    Update AnimalRepository.java

--- sgraa/src/main/java/com/sgraa/repository/AnimalRepository.java
+++ sgraa/src/main/java/com/sgraa/repository/AnimalRepository.java
@@ -2,6 +2,10 @@ package com.sgraa.repository;
 
 import com.sgraa.model.Animal;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+/**
+ * Interface que define o repositório para a entidade "AnimalRepository".
+ * Esta interface estende JpaRepository, fornecendo métodos CRUD padrão e permitindo a criação de consultas personalizadas.
+ * Repositórios são responsáveis por abstrair o acesso ao banco de dados e fornecer operações de persistência para a entidade.
+ */
 public interface AnimalRepository extends JpaRepository<Animal, Long> {
 }
