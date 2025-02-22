01bb87a9aff60937389985c97d0130ac990abcd0
Mateus Araujo <163949052+MQUARTZO@users.noreply.github.com>
2025-02-22 07:50:18 -0300

    Update AdocaoRepository.java

--- sgraa/src/main/java/com/sgraa/repository/AdocaoRepository.java
+++ sgraa/src/main/java/com/sgraa/repository/AdocaoRepository.java
@@ -4,7 +4,11 @@ import com.sgraa.model.Adocao;
 import com.sgraa.model.Animal;
 import com.sgraa.model.Pretendente;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+/**
+ * Interface que define o repositório para a entidade "AdocaoRepository".
+ * Esta interface estende JpaRepository, fornecendo métodos CRUD padrão e permitindo a criação de consultas personalizadas.
+ * Repositórios são responsáveis por abstrair o acesso ao banco de dados e fornecer operações de persistência para a entidade.
+ */
 public interface AdocaoRepository extends JpaRepository<Adocao, Long> {
     boolean existsByAnimalAndPretendente(Animal animal, Pretendente pretendente);
 }
