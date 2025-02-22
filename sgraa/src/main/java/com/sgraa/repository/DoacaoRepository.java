a73b5aa941205b5b5a39647a164560e4b9107b0a
Mateus Araujo <163949052+MQUARTZO@users.noreply.github.com>
2025-02-22 07:51:21 -0300

    Update DoacaoRepository.java

--- sgraa/src/main/java/com/sgraa/repository/DoacaoRepository.java
+++ sgraa/src/main/java/com/sgraa/repository/DoacaoRepository.java
@@ -2,6 +2,10 @@ package com.sgraa.repository;
 
 import com.sgraa.model.Doacao;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+/**
+ * Interface que define o repositório para a entidade "DoacaoRepository".
+ * Esta interface estende JpaRepository, fornecendo métodos CRUD padrão e permitindo a criação de consultas personalizadas.
+ * Repositórios são responsáveis por abstrair o acesso ao banco de dados e fornecer operações de persistência para a entidade.
+ */
 public interface DoacaoRepository extends JpaRepository<Doacao, Long> {
 }
