a4697df464ec588eee1dfd51b4a8ed9878ac2360
Mateus Araujo <163949052+MQUARTZO@users.noreply.github.com>
2025-02-22 07:52:15 -0300

    Update EstoqueRepository.java

--- sgraa/src/main/java/com/sgraa/repository/EstoqueRepository.java
+++ sgraa/src/main/java/com/sgraa/repository/EstoqueRepository.java
@@ -3,7 +3,11 @@ package com.sgraa.repository;
 import com.sgraa.model.Estoque;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
-
+/**
+ * Interface que define o repositório para a entidade "EstoqueRepository".
+ * Esta interface estende JpaRepository, fornecendo métodos CRUD padrão e permitindo a criação de consultas personalizadas.
+ * Repositórios são responsáveis por abstrair o acesso ao banco de dados e fornecer operações de persistência para a entidade.
+ */
 public interface EstoqueRepository extends JpaRepository<Estoque, Long> {
     Optional<Estoque> findByItem(String item);
 }
