da73c7f8fd70aa3a2052bdbea6ecc1aadc94504d
Mateus Araujo <163949052+MQUARTZO@users.noreply.github.com>
2025-02-22 07:36:26 -0300

    Update FuncaoVoluntario.java

--- sgraa/src/main/java/com/sgraa/model/FuncaoVoluntario.java
+++ sgraa/src/main/java/com/sgraa/model/FuncaoVoluntario.java
@@ -1,6 +1,21 @@
+// Define o pacote onde esta enumeração está localizada
 package com.sgraa.model;
 
+/**
+ * Enumeração que representa as funções que um voluntário pode exercer no sistema.
+ * Cada constante define um papel específico que um voluntário pode assumir.
+ */
 public enum FuncaoVoluntario {
+
+    /**
+     * Função de resgate de animais.
+     * Voluntários com essa função são responsáveis por resgatar animais em situações de risco.
+     */
     RESGATE,
+
+    /**
+     * Função de assistência aos animais.
+     * Voluntários com essa função são responsáveis por cuidar, alimentar e fornecer suporte geral aos animais abrigados.
+     */
     ASSISTENCIA
 }
